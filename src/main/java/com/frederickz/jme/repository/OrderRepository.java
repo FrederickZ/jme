6c73848621ef237976fa05f069ccee6a0467d64f
Frederick Zhu <zhu.yuming@outlook.com>
2021-09-10 19:06:37 -0500

    changed to JpaRepository

--- src/main/java/com/frederickz/jme/repository/OrderRepository.java
+++ src/main/java/com/frederickz/jme/repository/OrderRepository.java
@@ -1,7 +1,7 @@
 package com.frederickz.jme.repository;
 
 import com.frederickz.jme.model.Order;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface OrderRepository extends CrudRepository<Order, Long> {
+public interface OrderRepository extends JpaRepository<Order, Long> {
 }
--- src/main/java/com/frederickz/jme/repository/UserRepository.java
+++ src/main/java/com/frederickz/jme/repository/UserRepository.java
@@ -1,7 +1,7 @@
 package com.frederickz.jme.repository;
 
 import com.frederickz.jme.model.User;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends CrudRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, Long> {
 }
